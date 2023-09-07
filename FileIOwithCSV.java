e8f122a6072adb5a92f5b99c1278c99d467477e1
dmohle <10038913+dmohle@users.noreply.github.com>
2023-09-07 12:13:06 -0700

    Add files via upload

---
+++ FileIOwithCSV.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+
+public class FileIOwithCSV {
+    public static void main(String[] args) throws IOException {
+        System.out.println("\n\n Welcome to File I/O ! \n\n");
+
+        String pathName = "C:\\Users\\BE218\\javaFiles\\arrivingAnimals.txt";
+        String pathOutput = "C:\\Users\\BE218\\javaFiles\\myNewFile.txt";
+        BufferedReader reader = null;
+        String line = "";
+
+        // Create a File Writer.
+        BufferedWriter writer = new BufferedWriter(new FileWriter(pathOutput));
+
+        try {
+            reader = new BufferedReader(new FileReader(pathName));
+            while ( (line = reader.readLine()) != null ) {
+                String[] row = line.split(",");
+
+                for (String i : row) {
+                    System.out.println("\n" + i);
+                    writer.write("\n" + i);
+                }
+
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            reader.close();
+            writer.close();
+        }
+
+    }
+}
